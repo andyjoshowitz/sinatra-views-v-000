177c56ad5b489bf36c0451fcb56f69d064922874
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-03-21 18:12:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
+<<<<<<< HEAD
 		erb :index
+=======
+		"<h1>Hello World</h1>"
+>>>>>>> 23e4537ccc1d118e9ab5a1545e17d7d37c356a30
 	end
 	
 	get "/info" do
